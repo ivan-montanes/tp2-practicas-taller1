a03c32516eef8d48cd80c02acd032144b115e159
ivan montanes <ivanmontanesfilipit@gmail.com>
2024-11-22 21:13:06 -0300

    punto 2 terminado

--- tp2-ejercicio1/Program.cs
+++ tp2-ejercicio1/Program.cs
@@ -1,3 +1,22 @@
-﻿using EspacioCalculadora;
-EspacioCalculadora.Calculadora calculadora1 = new EspacioCalculadora.Calculadora();
-calculadora1.encadenarOperaciones();
+﻿using EspacioEmpleado;
+Empleado empleado1 = new Empleado("ivan","montañes",new DateTime(2005,1,27),'s',new DateTime(2021,5,5),870000,Cargo.Investigador);
+Empleado empleado2 = new Empleado("santiago","gonzales",new DateTime(2004,2,14),'s',new DateTime(2019,3,8),1350000,Cargo.Ingeniero);
+Empleado empleado3 = new Empleado("mateo","leiva",new DateTime(2003,2,13),'s',new DateTime(2023,5,10),675000,Cargo.Auxiliar);
+Empleado[] empleados = [empleado1, empleado2, empleado3];
+//obtenemos lo que se paga de salarios, y el empleado mas cercano a jubilarse//
+double gastoTotal = 0.0;
+Empleado masCercano = null;
+int restantes = 100000000;
+foreach(Empleado empleado in empleados){
+    gastoTotal += empleado.ObtenerSalario();
+    if(empleado.ObtenerRestantesJubilacion() < restantes){
+        restantes = empleado.ObtenerRestantesJubilacion();
+        masCercano = empleado;
+    }
+}
+Console.WriteLine($"Total Pagado En Concepto De Salarios ${gastoTotal}");
+Console.WriteLine("----------EMPLEADO MAS CERCANO A JUBILARSE----------");
+Console.WriteLine($"Nombre Completo: {masCercano.Nombre} {masCercano.Apellido}");
+Console.WriteLine($"Edad: {masCercano.ObtenerEdad()}");
+Console.WriteLine($"Cantidad de años para jubilarse: {masCercano.ObtenerRestantesJubilacion()}");
+Console.WriteLine($"Salario Correspondiente {masCercano.ObtenerSalario()}");
--- tp2-ejercicio1/clases/Empleado.cs
+++ tp2-ejercicio1/clases/Empleado.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+
 namespace EspacioEmpleado;
 
 //definimos la variable de tipo enum para guardar los cargos//
@@ -10,23 +12,97 @@ public enum Cargo
     Investigador
 }
 
-class empleado{
+class Empleado{
     //definimos los campos//
     private string nombre;
     private string apellido;
     private DateTime fechaNac;
     private char estadoCivil;
+    private DateTime fechaIngreso;
     private double sueldoBasico;
     private Cargo cargo;
+
+    public Empleado(string nombre, string apellido, DateTime fechaNac, char estadoCivil, DateTime fechaIngreso, double sueldoBasico, Cargo cargo)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        FechaNac = fechaNac;
+        EstadoCivil = estadoCivil;
+        FechaIngreso = fechaIngreso;
+        SueldoBasico = sueldoBasico;
+        Cargo = cargo;
+    }
 
     //definimos las propiedades//
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apellido { get => apellido; set => apellido = value; }
     public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
     public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
     public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
     public Cargo Cargo { get => cargo; set => cargo = value; }
-
     
+
+    //definimos los metodos//
+
+    //metodo privado para calcular las fechas//
+    private int CalcularDiferencia(DateTime FechaEvaluada){
+        //usamos la fecha actual para calcular la antiguedad//
+        DateTime fechaActual = DateTime.Now;
+        int antiguedad = 0;
+        //logica para calcular la antiguedad//
+        antiguedad = fechaActual.Year - FechaEvaluada.Year;
+        if(FechaEvaluada.Month > fechaActual.Month){
+            antiguedad--;
+        }else if(fechaActual.Month == FechaEvaluada.Month){
+            if(FechaEvaluada.Day > fechaActual.Day){
+                antiguedad--;
+            }
+        }
+        return antiguedad;
+    }
+    public int ObtenerAntiguedad(){
+        int antiguedad = CalcularDiferencia(FechaIngreso);
+        return antiguedad;
+    }
+
+    public int ObtenerEdad(){
+        int edad = CalcularDiferencia(FechaNac);
+        return edad;
+    }
+
+    public int ObtenerRestantesJubilacion(){
+        int restantes = 65 - CalcularDiferencia(FechaNac);
+        return restantes;
+    }
+
+    //metodo privado para calcular el adicional//
+    private double CalcularAdicional(){
+        double adicional = 0;
+        //adicional por la antiguedad//
+        if(CalcularDiferencia(FechaIngreso) <= 20){
+            adicional += (SueldoBasico / 100) * CalcularDiferencia(FechaIngreso);
+        }else{
+            adicional += (sueldoBasico / 100) * 25;
+        }
+
+        //adicional segun el cargo//
+        if(Cargo == Cargo.Ingeniero || Cargo == Cargo.Especialista){
+            adicional *= 1.5;
+        }
+
+        //adicional segun el estado civil//
+        if(EstadoCivil == 'C' || estadoCivil == 'c'){
+            adicional += 150000;
+        }
+
+        return adicional;
+    }
+    public double ObtenerSalario(){
+        double adicional = CalcularAdicional();
+        double salario = adicional + SueldoBasico;
+        return salario;
+    }
+
 
 }
